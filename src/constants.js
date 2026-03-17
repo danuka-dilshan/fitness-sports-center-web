a9e4536532fa28c0dc7d405ff17cde517e50dec5
danuka-dilshan <hdd.dilshankumarasiri@gmail.com>
2026-03-17 20:08:50 +0530

    feat: constants, theme & global styles

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const LOGO = "data:image/jpeg;base64,..."; // full base64
+
+export const C = {
+  gold:    "#CFA010",
+  goldDim: "#A07C08",
+  brown:   "#231F0E",
+  black:   "#080808",
+  charcoal:"#2E2E2E",
+  offwhite:"#F2EFE4",
+  gray:    "#6B6B60",
+};
